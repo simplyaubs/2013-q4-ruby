6860a49648d0e022697691b98dea8cb6a964db60
Aubrey Howell <simplyaubs@gmail.com>
2013-10-15 16:08:37 -0600

    Create seeds

---
+++ 206-athletes-x-races/db/seeds.rb
@@ -0,0 +1,8 @@
+Race.create! name: "100-meter dash"
+Race.create! name: "10K run"
+Race.create! name: "400-meter dash"
+
+Runner.create! name: "Amy"
+Runner.create! name: "Eric"
+Runner.create! name: "John"
+Runner.create! name: "Lisa"
