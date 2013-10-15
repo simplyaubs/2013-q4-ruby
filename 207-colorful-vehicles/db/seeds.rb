58e67d65321bedd6aaf181f7d3b60e3ac51be840
Aubrey Howell <simplyaubs@gmail.com>
2013-10-15 15:16:06 -0600

    Add seeds.rb

--- 207-colorful-vehicles/db/seeds.rb
+++ 207-colorful-vehicles/db/seeds.rb
@@ -1,1 +1,9 @@
-# TODO: write this
+Color.create! name: "fuschia", html_color_code: "#AA0078"
+Color.create! name: "green", html_color_code: "#2C6700"
+Color.create! name: "purple", html_color_code: "#6600CC"
+Color.create! name: "yellow", html_color_code: "#FFCC00"
+
+Vehicle.create! name: "airplane", image_filename: "plane.png"
+Vehicle.create! name: "bicycle", image_filename: "bike.png"
+Vehicle.create! name: "bus", image_filename: "bus.png"
+Vehicle.create! name: "car", image_filename: "car.png"
