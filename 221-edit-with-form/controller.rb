9b0ea1887424b005ddc3762ba209a966850db3e5
Aubrey Howell <simplyaubs@gmail.com>
2013-10-24 23:43:04 -0600

    Exercise 221 edits

--- 221-edit-with-form/controller.rb
+++ 221-edit-with-form/controller.rb
@@ -11,4 +11,12 @@ get "/patients/:id" do
   halt erb(:edit)
 end
 
-# TODO: write POST handler
+post "/patients/:id" do
+	id                 = params["id"]
+	@patient           = Patient.find(id)
+	@patient.systolic  = params["systolic"]
+	@patient.diastolic = params["diastolic"]
+	@patient.save!
+
+	redirect "/"
+end
