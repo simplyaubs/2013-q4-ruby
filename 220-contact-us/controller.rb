994bb9a96b37a4443967ba1713f9abfa6047a7f1
Aubrey Howell <simplyaubs@gmail.com>
2013-10-24 23:42:40 -0600

    Create contact us page

--- 220-contact-us/controller.rb
+++ 220-contact-us/controller.rb
@@ -4,4 +4,11 @@ get "/" do
   halt erb(:index)
 end
 
-# TODO: write POST handler
+post "/" do
+	@message         = Message.new
+	@message.subject = params["subject"]
+	@message.body    = params["body"]
+	@message.save!
+
+	redirect "/"
+end
