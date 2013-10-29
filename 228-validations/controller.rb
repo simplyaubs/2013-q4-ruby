d2424c29dd25240025ffc9dee5415d8451e63d47
Aubrey Howell <simplyaubs@gmail.com>
2013-10-29 17:33:38 -0600

    Validations controller, model, view

--- 228-validations/controller.rb
+++ 228-validations/controller.rb
@@ -6,14 +6,19 @@ get "/" do
 end
 
 get "/new_user" do
+	@user = User.new
   halt erb(:new)
 end
 
 post "/new_user" do
-  user            = User.new
-  user.email      = params["email"]
-  user.first_name = params["first_name"]
-  user.age        = params["age"]
-  user.save!
-  redirect "/"
+  @user            = User.new
+  @user.email      = params["email"]
+  @user.first_name = params["first_name"]
+  @user.age        = params["age"]
+
+  if @user.save == true
+  	redirect "/"
+	else
+		halt erb(:new)
+	end
 end
--- 228-validations/models/user.rb
+++ 228-validations/models/user.rb
@@ -1,2 +1,5 @@
 class User < ActiveRecord::Base
+	validates :email, presence: {
+		message: "must not be blank."
+	} 
 end
