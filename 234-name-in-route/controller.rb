1f77aca3d9c597cfeef21ee87560197186718374
Aubrey Howell <simplyaubs@gmail.com>
2013-11-04 08:55:33 -0700

    234 - Name in Route add get for "/" in controller

--- 234-name-in-route/controller.rb
+++ 234-name-in-route/controller.rb
@@ -8,3 +8,9 @@ end
 # TODO: write GET handler to serve show.html.erb with @entry set
 # to the correct Encyclopedia Entry for routes like /albatross 
 # and /jay
+
+get "/:id" do
+	bird = params[:id]
+	@entry = EncyclopediaEntry.find_by(word: bird)
+	halt erb(:show)
+end
