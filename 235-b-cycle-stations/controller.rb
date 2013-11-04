2c156d5810730ed02e41fe18f732e2af6a811d89
Aubrey Howell <simplyaubs@gmail.com>
2013-11-04 08:56:04 -0700

    235 - b-cycle stations - add links to controller

--- 235-b-cycle-stations/controller.rb
+++ 235-b-cycle-stations/controller.rb
@@ -7,25 +7,41 @@ end
 
 get "/existing" do
   # TODO: Assign to @stations variable
+
+  @stations = Station.where(status: "existing")
+
   halt erb(:index)
 end
 
 get "/private" do
   # TODO: Assign to @stations variable
+
+  @stations = Station.where(property_type: "private")
+
   halt erb(:index)
 end
 
 get "/zip/:zip_code" do
   # TODO: Assign to @stations variable
+
+  zip_code = params["zip_code"]
+  @stations = Station.where(zip_code: zip_code)
+
   halt erb(:index)
 end
 
 get "/street/Broadway" do
   # TODO: Assign to @stations variable
+
+  @stations = Station.where("station_address ilike ?", "%broadway%")
+
   halt erb(:index)
 end
 
 get "/street/17th" do
   # TODO: Assign to @stations variable
+
+  @stations = Station.where("station_address ilike ?", "%17th%")
+
   halt erb(:index)
 end
